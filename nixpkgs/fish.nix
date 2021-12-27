00764b40a3cda8958e69751b63870fedb8b3358e
mutualex <luke@luke.rip>
2021-12-26 20:47:58 -0500

    added github co-pilot

--- nixpkgs/fish.nix
+++ nixpkgs/fish.nix
@@ -3,35 +3,35 @@
 {
   programs.fish = {
     enable = true;
-	
+
     # Gruvbox colors.
     shellInit = ''
-        set -U fish_color_autosuggestion 504945
-        set -U fish_color_cancel -r
-        set -U fish_color_command green #white
-        set -U fish_color_comment 504945
-        set -U fish_color_cwd green
-        set -U fish_color_cwd_root red
-        set -U fish_color_end brblack #blue
-        set -U fish_color_error red
-        set -U fish_color_escape yellow #green
-        set -U fish_color_history_current --bold
-        set -U fish_color_host normal
-        set -U fish_color_match --background=brblue
-        set -U fish_color_normal normal
-        set -U fish_color_operator blue #green
-        set -U fish_color_param bdae93
-        set -U fish_color_quote yellow #brblack
-        set -U fish_color_redirection cyan
-        set -U fish_color_search_match bryellow --background=504945
-        set -U fish_color_selection white --bold --background=504945
-        set -U fish_color_status red
-        set -U fish_color_user brgreen
-        set -U fish_color_valid_path --underline
-        set -U fish_pager_color_completion normal
-        set -U fish_pager_color_description yellow --dim
-        set -U fish_pager_color_prefix white --bold #--underline
-        set -U fish_pager_color_progress brwhite --background=cyan
+      set -U fish_color_autosuggestion 504945
+      set -U fish_color_cancel -r
+      set -U fish_color_command green #white
+      set -U fish_color_comment 504945
+      set -U fish_color_cwd green
+      set -U fish_color_cwd_root red
+      set -U fish_color_end brblack #blue
+      set -U fish_color_error red
+      set -U fish_color_escape yellow #green
+      set -U fish_color_history_current --bold
+      set -U fish_color_host normal
+      set -U fish_color_match --background=brblue
+      set -U fish_color_normal normal
+      set -U fish_color_operator blue #green
+      set -U fish_color_param bdae93
+      set -U fish_color_quote yellow #brblack
+      set -U fish_color_redirection cyan
+      set -U fish_color_search_match bryellow --background=504945
+      set -U fish_color_selection white --bold --background=504945
+      set -U fish_color_status red
+      set -U fish_color_user brgreen
+      set -U fish_color_valid_path --underline
+      set -U fish_pager_color_completion normal
+      set -U fish_pager_color_description yellow --dim
+      set -U fish_pager_color_prefix white --bold #--underline
+      set -U fish_pager_color_progress brwhite --background=cyan
     '';
   };
 }
--- nixpkgs/neovim/plugins.nix
+++ nixpkgs/neovim/plugins.nix
@@ -1,5 +1,16 @@
 { config, pkgs, ... }:
 
+let
+  copilot = pkgs.vimUtils.buildVimPlugin {
+    name = "copilot";
+    src = pkgs.fetchFromGitHub {
+      owner = "github";
+      repo = "copilot";
+      rev = "c01314840b94da0b9767b52f8a4bbc579214e509";
+      sha256 = "10vw2hjrg20i8id5wld8c5b1m96fnxvkb5qhbdf9w5sagawn4wc2";
+    };
+  };
+in
 {
   programs.neovim.plugins = with pkgs.vimPlugins; [
     vim-nix
@@ -13,5 +24,6 @@
     coc-rls
     coc-python
     gruvbox
+    copilot
   ];
 }
